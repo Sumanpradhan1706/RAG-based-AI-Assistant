ab3f5956aebe238a17d85434a34c5c207af2b606
Sumanpradhan1706 <pradhan.1.7.2006@gmail.com>
2025-10-22 21:07:30 +0530

    transcription and chunking functionality for audio processing

---
+++ create_chunks.py
@@ -0,0 +1,36 @@
+import whisper
+import json
+import os
+
+model = whisper.load_model("small")
+os.makedirs("jsons", exist_ok=True)
+
+
+audios = os.listdir("audios")
+
+for audio in audios:
+    print(audio)
+    if "_" in audio:
+        number = audio.split("_")[0]
+        title = audio.split("_")[1][:-4]
+    else:
+        title = audio.split(".")[0]  # fallback to filename without extension
+    print(number,title)
+    result = model.transcribe(audio=f"audios/{audio}",
+                          language="hi",
+                          task="translate",
+                          word_timestamps=False)
+
+
+    chunks = []
+    for segment in result["segments"]:
+        chunks.append({"number": number, "title" :title, "start": segment["start"], "end": segment["end"], "text": segment["text"]})
+
+    chunks_with_metadata = {
+        "chunks": chunks,
+        "text": result["text"]
+    }
+
+    with open(f"jsons/{audio}.json", "w") as f:
+        json.dump(chunks_with_metadata, f)
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+result = model.transcribe(audio ="audios/sample.mp3", language ="hi",
+      
